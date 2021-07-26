786105cb2f00fb41d7ded4e8c003e5da3442804a
Alex Fourmy <alexfourmy@gmail.com>
2021-07-26 09:42:53 +0200

    Implemented the CRUD feature for the database

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 let express = require('express')
-let MongoClient = require('mongodb').MongoClient
+let mongodb = require('mongodb').MongoClient
 
+let ObjectId  = require('mongodb').ObjectId
 let app = express()
 let db;
 
+app.use(express.static('public'))
+
 let connectionString = 'mongodb+srv://todoAppUser:Fender666@cluster0.wlxbg.mongodb.net/TodoApp?retryWrites=true&w=majority'
 
-MongoClient.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
     db = client.db()
     app.listen(3000)
 }) 
 
+app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', function(req, res) {
@@ -43,8 +47,8 @@ app.get('/', function(req, res) {
                         <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
                             <span class="item-text">${item.text}</span>
                             <div>
-                            <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-                            <button class="delete-me btn btn-danger btn-sm">Delete</button>
+                            <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+                            <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
                             </div>
                         </li>
                         `
@@ -52,6 +56,9 @@ app.get('/', function(req, res) {
                 </ul>
                 
             </div>
+
+            <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+            <script src="/browser.js" ></script>
             
             </body>
             </html>
@@ -62,5 +69,17 @@ app.get('/', function(req, res) {
 app.post('/create-item', function(req, res) {
     db.collection('items').insertOne({text: req.body.item}, function() {
         res.redirect('/')
+    })
+})
+
+app.post('/update-item', function(req, res) {
+    db.collection('items').findOneAndUpdate({_id: new ObjectId(req.body.id)}, {$set: {text: req.body.text}}, function() {
+        res.send("Success")
+    })
+})
+
+app.post('/delete-item', function(req, res){
+    db.collection('items').deleteOne({_id: new ObjectId(req.body.id)}, function() {
+        res.send("Success")
     })
 })
